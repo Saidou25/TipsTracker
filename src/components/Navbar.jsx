4e86fff424ca8c1a5959b166310e0b0b7c3ed679
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2025-01-17 14:22:57 -0500

    changed to= in navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,17 +13,17 @@ const Navbar = () => {
       <div className="container-nav">
         <ul className="nav">
           <li className="nav-link">
-            <NavLink className="nav-text bg-info" to="/tipstracker/enterTips">
+            <NavLink className="nav-text bg-info" to="/enterTips">
               enter Tips
             </NavLink>
           </li>
           <li className="nav-link">
-            <NavLink className="nav-text bg-info" to="/tipstracker/dashboard">
+            <NavLink className="nav-text bg-info" to="/dashboard">
               dashboard
             </NavLink>
           </li>
           <li className="nav-link">
-            <NavLink className="nav-text bg-info" to="/tipstracker/profile">
+            <NavLink className="nav-text bg-info" to="/profile">
               profile
             </NavLink>
           </li>
