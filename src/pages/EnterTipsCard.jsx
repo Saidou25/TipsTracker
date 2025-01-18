a6c92c12606d0c6d5d7ca47a4441695f8290ea9b
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2025-01-17 19:21:00 -0500

    formatted date

--- src/pages/EnterTipsCard.jsx
+++ src/pages/EnterTipsCard.jsx
@@ -23,12 +23,12 @@ const EnterTipsCard = ({ showSuccess }) => {
   // Getting the full length of the day
   const fullDayName = date.toLocaleString("en-US", { weekday: "long" });
   // Format the date as MM/DD/YYYY
-  const formattedDate = "01/15/2025"; 
-  // const formattedDate = date.toLocaleDateString("en-US", {
-  //   month: "2-digit",
-  //   day: "2-digit",
-  //   year: "numeric",
-  // });
+  // const formattedDate = "01/15/2025"; for testing
+  const formattedDate = date.toLocaleDateString("en-US", {
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric",
+  });
 
   const [updateCurrentUserCollection, setUpdateCurrentUserCollection] =
     useState(false);
